bd614f5ea7b07a07f02c1941872e2d4c110c0f1c
cilly <C19491104@mytudublin.ie>
2022-11-13 19:12:22 +0000

    added bar

--- FishTank/Assets/FishAI.cs
+++ FishTank/Assets/FishAI.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FishAI : MonoBehaviour
+public class FishAI : SpectrumMonitor
 {
-    public float radius = 10;
-    public int numWaypoints = 10;
+    public float radius = 5;
+    public int numWaypoints = 5;
     public int current = 0;
     List<Vector3> waypoints = new List<Vector3>();
     public float speed = 10;
-    public Transform player;    
+    public Transform player; 
+    public GameObject SwimRing;  
+   
+     
    
 
     public void OnDrawGizmos()
@@ -21,7 +24,7 @@ public class FishAI : MonoBehaviour
             for(int i = 0 ; i < numWaypoints ; i ++)
             {
                 float angle = theta * i;
-                Vector3 pos = new Vector3(Mathf.Sin(angle) * radius, 0, Mathf.Cos(angle) * radius);
+                Vector3 pos = new Vector3(Mathf.Sin(angle) * radius, 10, Mathf.Cos(angle) * radius);
                 pos = transform.TransformPoint(pos);
                 Gizmos.color = Color.red;
                 Gizmos.DrawWireSphere(pos, 1); 
@@ -31,7 +34,7 @@ public class FishAI : MonoBehaviour
 
     // Use this for initialization
     void Awake () {
-        //Create the list contianing the wayypoints
+        //Create the list contianing the wayypoints and spawn the fish rings
         float theta = (Mathf.PI * 2.0f) / numWaypoints;
         for(int i = 0 ; i < numWaypoints ; i ++)
         {
@@ -39,11 +42,15 @@ public class FishAI : MonoBehaviour
             Vector3 pos = new Vector3(Mathf.Sin(angle) * radius, 0, Mathf.Cos(angle) * radius);
             pos = transform.TransformPoint(pos);
             waypoints.Add(pos); 
+            float angleDegrees = -angle*Mathf.Rad2Deg;
+            Quaternion rot = Quaternion.Euler(0, angleDegrees, 0);
+            Instantiate(SwimRing, pos, rot);
         }
     }
 
     // Update is called once per frame
     void Update () {
+        base.Update();
         Vector3 pos = transform.position;
         Vector3 toNext = waypoints[current] - pos;
         float dist = toNext.magnitude;
@@ -52,11 +59,25 @@ public class FishAI : MonoBehaviour
             current = (current + 1) % waypoints.Count;
         }
         Vector3 direction = toNext / dist;
-        transform.position = Vector3.Lerp(transform.position, waypoints[current], Time.deltaTime);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(toNext, Vector3.up), 180 * Time.deltaTime);
-
-
+        if (isBeat = true)
+        {
+            transform.position = Vector3.Slerp(transform.position, waypoints[current], Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(toNext, Vector3.up), 180 * Time.deltaTime);
+        }
         
 
-}
+
+    }
+
+     public override void Beat()
+	{
+        base.Beat();
+        Debug.Log("fish can move");
+		
+        
+        
+
+      
+	}
+
 }
