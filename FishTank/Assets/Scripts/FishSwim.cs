3e3d78c4e2a0c6df5245743e7a93943d7182d0c7
cilly <C19491104@mytudublin.ie>
2022-11-20 17:29:25 +0000

    save

--- FishTank/Assets/Scripts/FishSwim.cs
+++ FishTank/Assets/Scripts/FishSwim.cs
@@ -2,32 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FishSwim : MonoBehaviour
-{
-    enum fishStates
-    { 
-        idle,
-        swimming
-    }
+public class FishSwim: MonoBehaviour {
+  public Transform Fish; 
+  private float waitTimer = 0.0f;
+  public int canMove = 1;
+  
+  public string SpawnPointTag = "FishSpawnPoint";
 
-    // Start is called before the first frame update
-    void Start()
+  void Start() {
+      
+    
+    
+    // transform.position = Vector3.Slerp(transform.position, waypoints[current], Time.deltaTime);
+    // transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(toNext, Vector3.up), 180 * Time.deltaTime);
+
+  }
+
+  void Update() 
+  {
+      if (canMove == 1)
+      {
+          StartCoroutine(Swim());
+      }
+      else 
+      {
+          //lblala
+      }
+    
+     
+  }
+
+  IEnumerator Swim()
     {
+        Vector3 startingPos  = transform.position;
+        Vector3 finalPos = RandomPoint();
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        switch (curState)
+        float elapsedTime = 0;
+     
+        while (transform.position != finalPos)
         {
-            case fishStates.swimming:
-                Swim();
-                break;
-            case fishStates.idle:
-                Idle();
-                break;
-        } 
+            transform.position = Vector3.Slerp(transform.position, finalPos, Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(finalPos, Vector3.up), 180 * Time.deltaTime);
+            
+        }
+        canMove = 0;
+        yield return null;
         
     }
+
+
+
+  Vector3 RandomPoint() 
+  {
+    GameObject[] SpawnPoints = GameObject.FindGameObjectsWithTag(SpawnPointTag);
+    int randomIndex = Random.Range(0, SpawnPoints.Length);
+    Vector3 randomPoint = SpawnPoints[randomIndex].transform.position;
+    return randomPoint;
+  }
 }
