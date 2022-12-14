6265aef117ebb25ba43d305c8f3004577b744049
cilly <C19491104@mytudublin.ie>
2022-12-14 15:38:59 +0000

    fixed VR in fish controller

--- FishTank/Assets/Scripts/PlayerFishCamera.cs
+++ FishTank/Assets/Scripts/PlayerFishCamera.cs
@@ -14,7 +14,6 @@ public class PlayerFishCamera : MonoBehaviour
     void LateUpdate()
     {
         transform.position = player.transform.position - player.transform.forward * FOV;
-        transform.LookAt (player.transform.position);
         transform.position = new Vector3 (transform.position.x , transform.position.y +1, transform.position.z );
     }
 }
